098972b7ff97dd27865fba2ebe5ab77d03530ec2
VALERIIA MUSIENKO <98159073+Valeria378@users.noreply.github.com>
2023-01-30 23:47:37 +0200

    added new lesson

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict" ;
+
+
+
+// alert("Hello world") ;
+// alert("goodbay");
+
+// confirm("yes?")
+
+
+//  let box = "Cat" ;
+
+//  alert ( "box" +box);
+
+const box = "lion" ;
+// box = "dog";
+
+alert (box);
